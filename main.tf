01e9b1cba7c563bb3f3b571dd56da814e4db65fb
raju-gautham <87421270+raju-gautham@users.noreply.github.com>
2021-07-14 16:51:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,63 @@
 provider "azurerm" {
   features {}
   subscription_id = "3c990c80-ea36-4c6c-afb2-da328349c3ac"
-  client_id = "acd1e968-cf26-43c7-9ed8-fa583e44d29b"
-  client_secret = "Q43el3S6Gjs-~6i~LnQbGUj3qafZhg7P-j"
-  tenant_id = "4c0495a4-a1a3-4901-8cc1-9f37bab59629"
+  client_id       = "acd1e968-cf26-43c7-9ed8-fa583e44d29b"
+  client_secret   = "Q43el3S6Gjs-~6i~LnQbGUj3qafZhg7P-j"
+  tenant_id       = "4c0495a4-a1a3-4901-8cc1-9f37bab59629"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources-xya"
+
+resource "azurerm_resource_group" "azgroup" {
+  name     = "snowres"
   location = "Central India"
 }
 
-module "linuxservers" {
-  source              = "Azure/compute/azurerm"
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  vm_os_simple        = "UbuntuServer"
-  public_ip_dns       = ["mynewdnster"] // change to a unique name per datacenter region
-  vnet_subnet_id      = module.network.vnet_subnets[0]
 }
 
-module "network" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
-  subnet_prefixes     = ["10.0.1.0/24"]
-  subnet_names        = ["subnet1"]
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
-output "linux_vm_public_name" {
-  value = module.linuxservers.public_ip_dns_name
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
 }
