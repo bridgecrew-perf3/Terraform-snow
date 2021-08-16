d200338dee7ae46016950b6921584a583c72f3af
raju-gautham <87421270+raju-gautham@users.noreply.github.com>
2021-08-16 23:12:52 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "2.72.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
